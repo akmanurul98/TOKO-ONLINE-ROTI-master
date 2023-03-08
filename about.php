8656ca512cd1d29042907c9e9bfd2cfec6f2382d
akmanurul375@gmail.com <akmanurul375@gmail.com>
2023-03-08 19:39:27 +0700

    first commit

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #ff8680"><b>Tentang Kami</b></h2>
+	<p class="text-justify">Akma Cake & Bakery adalah rantai toko roti Indonesia terkemuka dengan 17 cabang (dapur pusat) yang mengelola lebih dari 170 outlet: Bandung Raya (Kota Bandung, Kab. Bandung, Kab. Bandung Barat, Kota Cimahi), Jabodetabek (Jakarta, Bogor, Depok, Tangerang, Bekasi), dan Priangan Timur (Sumedang, Garut, Tasik, Ciamis, Banjar, Pangandaran). Kami masih terus memperluas secara nasional ke kota-kota lain.</p>
+<p>
+Akma Cake & Bakery  adalah salah satu pelopor pertama dalam bisnis roti modern di Indonesia. Didirikan pada tahun 2017, saat ini dikelola di bawah PT. Hoyong Untung Alim Rugi. Produk kami sehat, bergizi, dan terjangkau oleh semua orang.</p>
+</div>
+
+
+
+
+ <?php 
+	include 'footer.php';
+ ?>
